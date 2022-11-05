2bcc9030887dd91d758744ed45bdf749cfe77903
Sage <94207840+Sages0ft@users.noreply.github.com>
2022-11-06 00:37:16 +0100

    Sample for player data

--- api.lua
+++ api.lua
@@ -1,3 +1,14 @@
+Player = {}
+Player.__index = Player
+Player.Skills = {}
+Player.Skills[1] = 456
+
+function Player:get_skill(id)
+    return self.Skills[id]
+end
+
+print(Player:get_skill(1))
+
 -- Handler for packets to be sent to the server
 function on_send_packet(buf)
 end
