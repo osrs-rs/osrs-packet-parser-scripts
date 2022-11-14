3a18a85e1b07bacfbb32628a8db5a7cceaba65bc
Sages0ft <94207840+Sages0ft@users.noreply.github.com>
2022-11-14 18:01:14 +0100

    Write out packet

--- api.lua
+++ api.lua
@@ -1,4 +1,24 @@
-Player = {}
+local function print_packet(buf)
+    local data_str = "Sent packet: ["
+    for _,v in ipairs(buf) do
+        data_str =  data_str.."0x"..string.format("%X",v)..", "
+    end
+    data_str = data_str:sub(1, -3).."]"
+    print(data_str)
+end
+
+
+-- Handler for packets to be sent to the server
+function on_send_packet(buf)
+    print_packet(buf)
+end
+
+-- Handler for packets received from the server
+function on_recv_packet(buf)
+end
+
+--[[
+    Player = {}
 Player.__index = Player
 Player.Skills = {}
 Player.Skills[1] = 456
@@ -8,11 +28,4 @@ function Player:get_skill(id)
 end
 
 print(Player:get_skill(1))
-
--- Handler for packets to be sent to the server
-function on_send_packet(buf)
-end
-
--- Handler for packets received from the server
-function on_recv_packet(buf)
-end
+]]
